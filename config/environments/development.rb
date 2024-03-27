5117f526d96a41bdd9bf681d61292b99149ac179
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-03-27 08:03:50 +0000

    [Add] homesコントローラー Active_storage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   
   config.hosts << "5b16634e2a354a90a6b7124cfdb2591d.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.active_job.queue_adapter = :inline
 end
