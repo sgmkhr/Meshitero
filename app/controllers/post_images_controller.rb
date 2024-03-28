35cbdf78ab8f36f24c768f35c2ba59489f8826fd
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-03-28 08:31:02 +0000

    [finish]4-12

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -7,7 +7,7 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     @post_image.save
-    redirect_to post_image_path
+    redirect_to post_image_path(@post_image.id)
   end
 
   def index
@@ -15,6 +15,7 @@ class PostImagesController < ApplicationController
   end
 
   def show
+    @post_image = PostImage.find(params[:id])
   end
   
   private
