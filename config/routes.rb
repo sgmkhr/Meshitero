6d6ced66556c48dc3ca0f6b6c4ab4a9286080b7c
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-03-29 04:00:19 +0000

    [finish]4-14

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   resources :post_images, only: [:new, :create, :destroy, :index, :show]
   
   get 'homes/about' => 'homes#about', as: 'about'
+  
+  resources :users, only: [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
